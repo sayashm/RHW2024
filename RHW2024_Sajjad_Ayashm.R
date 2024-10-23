24847cde9c51702e757297a472c86e2af90f92ee
s.ayashm <s.ayashm@gmail.com>
2024-10-23 03:02:50 +0200

    until Question 5

--- RHW2024_Sajjad_Ayashm.R
+++ RHW2024_Sajjad_Ayashm.R
@@ -4,12 +4,73 @@
 mobility_data <- read.csv('mobility.csv', sep = ';', row.names = 1)
 mobility <- as.matrix(mobility_data, byrow= FALSE)
 
+# Question 1
 # Add and Use Matrix package
 library(Matrix)
 
 sparse_mobility <- Matrix(data = mobility, sparse = TRUE)
 
+
+# Question 2
 # Comparing size of mobility and sparse_mobility
 
 dence_size <- object.size(mobility); dence_size
 spare_size <- object.size(sparse_mobility); spare_size
+
+# Question 3
+# sparseProp function
+
+sparseProp <- function(x){
+  total_elements <- length(x)
+  
+  total_zero <- sum(x == 1)
+  
+  proportion <- total_zero / total_elements
+  return(proportion)
+}
+
+p <- sparseProp(sparse_mobility)
+print(p)
+
+
+# Question 4
+# Find Maximums in different indexes!
+
+index_matrix <- matrix(c('Div', 'DIV',
+                         'Art', 'ART',
+                         'Arm', 'ARM'),
+                       nrow = 3, ncol =2, byrow =TRUE)
+max_values <- mobility[as.matrix(index_matrix)]
+max_values
+
+
+# Question 5
+# Find frequencies of the values in a matrix!
+
+freqOfFreq <- function(x){
+  stopifnot("the input of function should be a matrix"=is.matrix(x))
+  
+  freq_table <- table(x, dnn = NULL)
+  return(freq_table)
+}
+
+freqOfFreq(mobility)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
